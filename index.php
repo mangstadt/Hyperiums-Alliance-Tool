fdcb3bd1385be2697991fa71b3e1e2abc495550d
Jerome Meyer <jerome@hollywoud.net>
2011-06-10 16:47:50 +0200

    Login page coding.

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 		<title>Hyperiums Alliance Tools</title>
 
 		<link rel="stylesheet" href="css/hat.less" type="text/less" />
+		
+		<link  href="http://fonts.googleapis.com/css?family=Metrophobic:regular" rel="stylesheet" type="text/css" />
 
 	</head>
 
@@ -19,15 +21,38 @@
 		<div id="main">
 			
 			<div id="header">
-				<div id="informations"></div>
-				<div id="actions"></div>
+				<div class="h100"></div>
 			</div>
 			
 			<div id="content">
 				
-			</div>
-			
-			<div id="footer">
+				<div id="login">
+					<div id="uL" class="topDiv"></div>
+					<div id="uC" class="topDiv oneMore">Authentication</div>
+					<div id="uR" class="topDiv"></div>
+					<div id="middle" class="centerDiv">
+						<div id="cL"></div>
+						<div id="cC">
+							<form id="loginForm" method="POST">
+								<label for="game_select_input">Game Selection</label>
+								<select name="game_select_input">
+									
+								</select>
+								<br/>
+								<label for="login_input">Your nickname</label>
+								<input type="text" name="login_input" id="login_input" value=""/>
+								<br/>
+								<label for="hkey_input">Your HAPI Key</label>
+								<input type="text" name="hkey_input" id="hkey_input" value=""/>
+								<br/>
+								<input type="submit" value="Login"/>
+							</form>
+						</div>
+						<div id="cR"></div>
+					</div>
+					<div id="bL" class="bottomDiv"></div>
+					<div id="bR" class="bottomDiv"></div>
+				</div>
 				
 			</div>
 			
