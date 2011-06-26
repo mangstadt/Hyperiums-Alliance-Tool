1beeb5a6ca25ee5886b2ab3353b722966ba94868
Mike Angstadt <mike.angstadt@gmail.com>
2011-06-26 15:13:06 -0400

    Fixed logout link.

--- admin/index.php
+++ admin/index.php
@@ -25,7 +25,7 @@ if (isset($_GET['logout'])){
 	if (isset($_SESSION['loggedIn'])):
 		?>
 		<a href="update-alliances.php">Update alliances</a> - Update the alliance info in the DB from HAPI alliance data files located in this dir.<br />
-		<a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>?logout">Logout</a> - Logout.<br />
+		<a href="index.php?logout">Logout</a> - Logout.<br />
 		<br />
 		<a style="background-color:#f99" href="wipe-db.php" onclick="return confirm('Are you sure you want to delete the entire database?  This cannot be undone.')">Wipe Database</a> - Wipe the database if you need to rebuild the schema.<br />
 		<?php
