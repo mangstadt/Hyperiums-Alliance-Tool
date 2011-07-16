7e126c8f41c45bf09f756b11fc3e406d61fac9b4
Mike Angstadt <mike.angstadt@gmail.com>
2011-07-16 13:51:45 -0400

    Upgraded PHP-HAPI to fix bug with creating the lock directory.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -3,7 +3,7 @@
  * This script automatically includes all classes.
  */
 
-require_once __DIR__ . '/PHP-HAPI-0.3.1.phar';
+require_once __DIR__ . '/PHP-HAPI-0.3.2.phar';
 
 spl_autoload_register(function ($class) {
 	$path = str_replace('\\', '/', $class); //replace back-slashes with forward-slashes
